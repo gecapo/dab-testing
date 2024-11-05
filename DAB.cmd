76c5e80392c50730841748f21e19a04987446d1e
Georgi Kalinkov <georgekalinkov@gmail.com>
2024-11-05 10:42:49 +0200

    Update DAB.cmd

--- DAB.cmd
+++ DAB.cmd
@@ -1,5 +1,5 @@
-@REM dab init --database-type "mssql" --host-mode "Development" --connection-string "Data Source=10.18.177.147,1433;Initial Catalog=Dev;User ID=Dev;Password=parolata;MultipleActiveResultSets=true;Pooling=True;TrustServerCertificate=true"
-@REM dab add Author --source "ATOS.MerchantData" --permissions "anonymous:*"
+@REM dab init --database-type "mssql" --host-mode "Development" --connection-string "Data Source=10.11.111.111,1433;Initial Catalog=Dev;User ID=Dev;Password=parolata;MultipleActiveResultSets=true;Pooling=True;TrustServerCertificate=true"
+@REM dab add Author --source "dbo.Data" --permissions "anonymous:*"
 
 @REM DAB_ENVIRONMENT=Developmenт
 SET DAB_ENVIRONMENT=Development
